4c16cdf1b29ed3ab0d905a28fbe2410bc3a3a2a6
jungae18 <46885199+jungae18@users.noreply.github.com>
2022-04-10 23:45:57 +0900

    Update val_dataset.py

--- CNN/dataset/COCO 2017/val_dataset.py
+++ CNN/dataset/COCO 2017/val_dataset.py
@@ -1,4 +1,5 @@
-import torch
+import wget
 
-torch.hub.download_url_to_file('https://ultralytics.com/assets/coco2017val.zip', 'tmp.zip')
-unzip -q tmp.zip -d ../datasets && rm tmp.zip
+coco_2017 = 'ttps://ultralytics.com/assets/coco2017val.zip'
+wget.download(coco_2017)
+unzip -q coco2017val.zip -d ../datasets && rm coco2017val.zip
