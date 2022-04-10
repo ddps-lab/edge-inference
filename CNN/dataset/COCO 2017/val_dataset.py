debe1bb8ef4c9f95f3fbb01dd8851ef9e1429c6f
jungae18 <46885199+jungae18@users.noreply.github.com>
2022-04-10 23:46:47 +0900

    Update val_dataset.py

--- CNN/dataset/COCO 2017/val_dataset.py
+++ CNN/dataset/COCO 2017/val_dataset.py
@@ -1,5 +1,5 @@
 import wget
 
-coco_2017 = 'ttps://ultralytics.com/assets/coco2017val.zip'
+coco_2017 = 'https://ultralytics.com/assets/coco2017val.zip'
 wget.download(coco_2017)
 unzip -q coco2017val.zip -d ../datasets && rm coco2017val.zip
