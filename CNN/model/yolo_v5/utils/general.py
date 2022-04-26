d0e1134fad73b63b9b40df43709c46700a56ecef
jungae18 <46885199+jungae18@users.noreply.github.com>
2022-04-26 14:22:45 +0900

    Update general.py

--- CNN/model/yolo_v5/utils/general.py
+++ CNN/model/yolo_v5/utils/general.py
@@ -32,8 +32,8 @@ import torch
 import torchvision
 import yaml
 
-from utils.downloads import gsutil_getsize
-from utils.metrics import box_iou, fitness
+from model.yolo_v5.utils.downloads import gsutil_getsize
+from model.yolo_v5.utils.metrics import box_iou, fitness
 
 # Settings
 FILE = Path(__file__).resolve()
