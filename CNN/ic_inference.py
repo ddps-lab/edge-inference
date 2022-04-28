4ac45991a63ef35ff2c1e46d089d2f11232d37ad
jungae18 <46885199+jungae18@users.noreply.github.com>
2022-04-28 11:27:13 +0900

    Update ic_inference.py

--- CNN/ic_inference.py
+++ CNN/ic_inference.py
@@ -101,7 +101,7 @@ def val_preprocessing(record):
     return image, label, label_text
 
 def get_dataset(batch_size, use_cache=False):
-    data_dir = './validation-00000-of-00001'
+    data_dir = './imagenet_[image count]'
     files = tf.io.gfile.glob(os.path.join(data_dir))
     dataset = tf.data.TFRecordDataset(files)
     
