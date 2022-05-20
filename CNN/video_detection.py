9d70f7e9e275beed72212cc105d342fdd361caaa
jungae18 <46885199+jungae18@users.noreply.github.com>
2022-05-20 13:38:59 +0900

    Update video_detection.py

--- CNN/video_detection.py
+++ CNN/video_detection.py
@@ -8,12 +8,12 @@ from pathlib import Path
 import torch
 import torch.backends.cudnn as cudnn
 
-from models.common import DetectMultiBackend
-from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
-from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+from model.yolo_v5.models.common import DetectMultiBackend
+from model.yolo_v5.utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from model.yolo_v5.utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
-from utils.plots import Annotator, colors, save_one_box
-from utils.torch_utils import select_device, time_sync
+from model.yolo_v5.utils.plots import Annotator, colors, save_one_box
+from model.yolo_v5.utils.torch_utils import select_device, time_sync
 
 
 def run(
