8058d8ab4a378012c1533045a3f7421dba3680c5
jungae18 <46885199+jungae18@users.noreply.github.com>
2022-07-15 12:09:24 +0900

    Update ic_inference_edgetpu.py
    dataset path update

--- CNN/ic_inference_edgetpu.py
+++ CNN/ic_inference_edgetpu.py
@@ -1,149 +1,109 @@
-import os
-import time
-import numpy as np
-import shutil
-import requests
-from functools import partial
 import argparse
-import tensorflow as tf
-import psutil
+import time
+import os
+import numpy as np
+
+from PIL import Image
+
+import classify
 import tflite_runtime.interpreter as tflite
+import platform
+
+EDGETPU_SHARED_LIB = {
+  'Linux': 'libedgetpu.so.1',
+  'Darwin': 'libedgetpu.1.dylib',
+  'Windows': 'edgetpu.dll'
+}[platform.system()]
 
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-  tf.config.experimental.set_virtual_device_configuration(gpus[0],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=0.3*1024)])
+def load_labels(path, encoding='utf-8'):
+  with open(path, 'r', encoding=encoding) as f:
+    lines = f.readlines()
+    if not lines:
+      return {}
+
+    if lines[0].split(' ', maxsplit=1)[0].isdigit():
+      pairs = [line.split(' ', maxsplit=1) for line in lines]
+      return {int(index): label.strip() for index, label in pairs}
+    else:
+      return {index: line.strip() for index, line in enumerate(lines)}
 
 
-results = None
-parser = argparse.ArgumentParser()
-parser.add_argument('--batchsize', default=1, type=int)
-parser.add_argument('--case', type=str, required=True)
-parser.add_argument('--engines',default=1, type=int)
-parser.add_argument('--img_size',default=224, type=int)
-args = parser.parse_args()
-batch_size = args.batchsize
-case=args.case
-num_engines=args.engines
-img_size=args.img_size
+def make_interpreter(model_file):
+  model_file, *device = model_file.split('@')
+  return tflite.Interpreter(
+      model_path=model_file,
+      experimental_delegates=[
+          tflite.load_delegate(EDGETPU_SHARED_LIB,
+                               {'device': device[0]} if device else {})
+      ])
 
 
-def deserialize_image_record(record):
-    feature_map = {'image/encoded': tf.io.FixedLenFeature([], tf.string, ''),
-                  'image/class/label': tf.io.FixedLenFeature([1], tf.int64, -1),
-                  'image/class/text': tf.io.FixedLenFeature([], tf.string, '')}
-    obj = tf.io.parse_single_example(serialized=record, features=feature_map)
-    imgdata = obj['image/encoded']
-    label = tf.cast(obj['image/class/label'], tf.int32)   
-    label_text = tf.cast(obj['image/class/text'], tf.string)   
-    return imgdata, label, label_text
+def load_data():
+    load_data=[]
+    image_path = './dataset/imagenet/imagenet_1000_raw'
+    input_files=os.listdir(image_path)
+    for image_file in input_files:
+        image = Image.open(image_path+'/'+ image_file)
+        load_data.append(image)
+    return load_data
 
 
-def val_preprocessing(record):
-    imgdata, label, label_text = deserialize_image_record(record)
-    label -= 1
-    image = tf.io.decode_jpeg(imgdata, channels=3, 
-                              fancy_upscaling=False, 
-                              dct_method='INTEGER_FAST')
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '-m', '--model', required=True, help='File path of .tflite file.')
+  parser.add_argument(
+      '-l', '--labels', help='File path of labels file.')
+  parser.add_argument(
+      '-k', '--top_k', type=int, default=1,
+      help='Max number of classification results')
+  parser.add_argument(
+      '-t', '--threshold', type=float, default=0.0,
+      help='Classification score threshold')
+  args = parser.parse_args()
 
-    shape = tf.shape(image)
-    height = tf.cast(shape[0], tf.float32)
-    width = tf.cast(shape[1], tf.float32)
-    side = tf.cast(tf.convert_to_tensor(256, dtype=tf.int32), tf.float32)
+  labels = load_labels(args.labels) if args.labels else {}
 
-    scale = tf.cond(tf.greater(height, width),
-                  lambda: side / width,
-                  lambda: side / height)
-    
-    new_height = tf.cast(tf.math.rint(height * scale), tf.int32)
-    new_width = tf.cast(tf.math.rint(width * scale), tf.int32)
-    
-    image = tf.image.resize(image, [new_height, new_width], method='bicubic')
-    image = tf.image.resize_with_crop_or_pad(image, img_size, img_size)
-    
-    image = tf.keras.applications.mobilenet.preprocess_input(image)
-    
-    return image, label, label_text
+  model_load_time = time.time()
+  interpreter = make_interpreter(args.model)
+  interpreter.allocate_tensors()
+  model_load_time = time.time() - model_load_time
 
-def get_dataset(batch_size, use_cache=False):
-    data_dir = './imagenet_1000'
-    files = tf.io.gfile.glob(os.path.join(data_dir))
-    dataset = tf.data.TFRecordDataset(files)
-    
-    dataset = dataset.map(map_func=val_preprocessing, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.batch(batch_size=batch_size)
-    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.repeat(count=1)
-    
-    if use_cache:
-        shutil.rmtree('tfdatacache', ignore_errors=True)
-        os.mkdir('tfdatacache')
-        dataset = dataset.cache(f'./tfdatacache/imagenet_val')
-    
-    return dataset
-    
+  dataset_load_time=time.time()
+  input_image=load_data()
+  dataset_load_time = time.time() - dataset_load_time
 
-def calibrate_fn(n_calib, batch_size, dataset):
-    for i, (calib_image, _, _) in enumerate(dataset):
-        if i > n_calib // batch_size:
-            break
-        yield (calib_image,)
+  iter_times=[]
+  accuracy=[]
 
 
-def predict_tflite(batch_size):
+  iftime_start = time.time()
+  for img in input_image: 
+      size = classify.input_size(interpreter)
+      image = img.convert('RGB').resize(size, Image.ANTIALIAS)
+      classify.set_input(interpreter, image)
+      start = time.perf_counter() 
+      interpreter.invoke()
+      iter_times.append(time.perf_counter() - start)
+      classes = classify.get_output(interpreter, args.top_k, args.threshold)
+
+      for klass in classes:
+          accuracy.append(klass.score)
+
+  inference_time = time.time() - iftime_start
+ 
    
-    model_load_time = time.time()
-    model = tflite.Interpreter("./yolov5s-fp16_edgetpu.tflite", experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
-    model.allocate_tensors()
-    model_load_time = time.time() - model_load_time
+  print('***** TF-lite matric *****')
+  print('accuracy =', np.sum(accuracy)/1000)
+  print('model_load_time =', model_load_time)
+  print('dataset_load_time =', dataset_load_time)
+  print('inference_time =', inference_time)
+  print('inference_time(avg) =', np.sum(iter_times) / 1000)
+  print('IPS =', 1000 / (model_load_time + dataset_load_time + inference_time))
+  print('IPS(inf) =', 1000 / np.sum(iter_times))
 
 
-    display_every = 5000
-    display_threshold = display_every
-    
-    pred_labels = []
-    actual_labels = []
-    iter_times = []
-    
-    dataset_load_time = time.time()
-    dataset = get_dataset(batch_size)
-    dataset_load_time = time.time() - dataset_load_time
-
-    input_details = model.get_input_details()[0]['shape']
-    output_details = model.get_output_details()[0]['shape']
-
-    
-    iftime_start = time.time()
-    for i, (validation_ds, batch_labels, _) in enumerate(dataset):
-        input_data = np.array(np.random.random_sample(validation_ds), dtype=np.float32)
-        start_time = time.time()
-        model.set_tensor(input_details[0]['shape'], input_data)
-        model.invoke()
-        pred_prob_keras=model.get_tensor(output_details)
-        iter_times.append(time.time() - start_time)
-
-        actual_labels.extend(label for label_list in batch_labels.numpy() for label in label_list)
-        pred_labels.extend(list(np.argmax(pred_prob_keras, axis=1)))
-        
-        if i*batch_size >= display_threshold:
-            display_threshold+=display_every
-            
-    iter_times = np.array(iter_times)
-    acc_keras_gpu = np.sum(np.array(actual_labels) == np.array(pred_labels))/len(actual_labels)
-    
-    
-    print('***** TF-lite matric *****')
-    print('user_batch_size =', batch_size)
-    print('accuracy =', acc_keras_gpu)
-    print('model_load_time =', model_load_time)
-    print('dataset_load_time =', dataset_load_time)
-    print('inference_time =', time.time() - iftime_start)
-    print('inference_time(avg) =', np.sum(iter_times) / (len(iter_times)*batch_size))
-    print('IPS =', (len(iter_times)*batch_size) / (model_load_time + dataset_load_time + (time.time() - iftime_start)))
-    print('IPS(inf) =', (len(iter_times)*batch_size) / np.sum(iter_times))
-
-
-
-if case == 'tflite' :
-    predict_tflite(batch_size)
+if __name__ == '__main__':
+  main()
