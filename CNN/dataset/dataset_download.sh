e04e969bb339e3763e099a34ba9d39dfb855cafe
jungae18 <46885199+jungae18@users.noreply.github.com>
2022-08-01 21:39:23 +0900

    Add files via upload
    dataset file 용량 문제로 인해 s3를 통해 download 하도록 스크립트 작성

---
+++ CNN/dataset/dataset_download.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+#image classification image dataset (tfrecord imagenet)
+mkdir imagenet
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_1000
+mv imagenet_1000 ./imagenet
+
+#image classification image dataset (raw imagenet)
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_metadata.txt
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/imagenet/imagenet_1000_raw.zip
+mv imagenet_metadata.txt ./imagenet
+unzip -q imagenet_1000_raw.zip -d ./imagenet && rm imagenet_1000_raw.zip
+
+
+#object detection image dataset(coco_2017)
+mkdir coco_2017
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/coco_2017/val_dataset.py
+python3 val_dataset.py
+mv val_dataset.py ./coco_2017
+unzip -q coco2017val.zip -d ../model/yolo_v5/datasets && rm coco2017val.zip
+
+
+#object detection video dataset
+mkdir video
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/dataset/video/road.mp4
+mv road.mp4 ./video
