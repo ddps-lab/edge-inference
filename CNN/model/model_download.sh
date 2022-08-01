66d685a58d5089818d2412ebe52dd2c50253ce9d
jungae18 <46885199+jungae18@users.noreply.github.com>
2022-08-01 21:48:45 +0900

    Add files via upload
    model file download 용량 문제로 인해 s3를 통해 download 할 수 있도록 스크립트 구성

---
+++ CNN/model/model_download.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+#image classification TF/FP32 model (mobilenet v1, mobilenet v2, inception v3)
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/mobilenet_v1/mobilenet_v1.zip
+unzip -q mobilenet_v1.zip -d ./mobilenet_v1 && rm mobilenet_v1.zip
+
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/mobilenet_v2/mobilenet_v2.zip
+unzip -q mobilenet_v2.zip -d ./mobilenet_v2 && rm mobilenet_v2.zip
+
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/inception_v3/inception_v3.zip
+unzip -q mobilenet_v2.zip -d ./mobilenet_v2 && rm mobilenet_v2.zip
+
+#image classification EdgeTPU tflite INT8 model (mobilenet v1, mobilenet v2, inception v3)
+mkdir mobilenet_v1_edgetpu_tflite
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/mobilenet_v1_edgetpu_tflite/tf2_mobilenet_v1_1.0_224_ptq_edgetpu.tflite
+mv tf2_mobilenet_v1_1.0_224_ptq_edgetpu.tflite ./mobilenet_v1_edgetpu_tflite
+
+mkdir mobilenet_v2_edgetpu_tflite
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/mobilenet_v2_edgetpu_tflite/tf2_mobilenet_v2_1.0_224_ptq_edgetpu.tflite
+mv tf2_mobilenet_v2_1.0_224_ptq_edgetpu.tflite ./mobilenet_v2_edgetpu_tflite
+
+mkdir inception_v3_edgetpu_tflite
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/inception_v3_edgetpu_tflite/inceptionv3_edgetpu.tflite
+mv inceptionv3_edgetpu.tflite ./inception_v3_edgetpu_tflite
+
+#object detection TF/FP32 model (yolo v5)
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/yolo_v5/yolo_v5.zip
+unzip -q yolo_v5.zip -d ./yolo_v5 && rm yolo_v5.zip
+
+#object detection EdgeTPU tflite INT8 model (yolo v5)
+mkdir yolo_v5_edgetpu_tflite
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/yolo_v5_edgetpu_tflite/yolov5s-int8_edgetpu.tflite
+mv yolov5s-int8_edgetpu.tflite ./yolo_v5_edgetpu_tflite 
+
+#object detection tflite FP16/INT8 model (yolo v5)
+mkdir yolo_v5_quantization_tflite
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/yolo_v5_quantization_tflite/yolov5s-fp16.tflite
+curl -O https://edge-inference.s3.us-west-2.amazonaws.com/CNN/model/yolo_v5_quantization_tflite/yolov5s-int8.tflite
+mv yolov5s-fp16.tflite yolov5s-int8.tflite ./yolo_v5_quantization_tflite
