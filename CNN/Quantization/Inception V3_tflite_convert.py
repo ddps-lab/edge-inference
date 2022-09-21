920bcc00e9a68b1f84471a970aa9bf0d377c3368
Jungae Park <46885199+jungae-park@users.noreply.github.com>
2022-09-21 12:02:11 +0900

    Create Inception V3_tflite_convert.py
    Inception V3 model tflite convert code

---
+++ CNN/Quantization/Inception V3_tflite_convert.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras.applications import inception_v3
+
+Inceptionv3_model = tf.keras.applications.InceptionV3(weights='imagenet')
+
+# A generator that provides a representative dataset
+def representative_data_gen():
+  dataset_list = tf.data.Dataset.list_files('./imagenet_1000_raw/*.JPEG')
+  for i in range(100):
+    image = next(iter(dataset_list))
+    image = tf.io.read_file(image)
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [299, 299])
+    image = tf.cast(image / 255., tf.float32)
+    image = tf.expand_dims(image, 0)
+    yield [image]
+
+converter = tf.lite.TFLiteConverter.from_keras_model(Inceptionv3_model)
+# This enables quantization
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# This sets the representative dataset for quantization
+converter.representative_dataset = representative_data_gen
+# This ensures that if any ops can't be quantized, the converter throws an error
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+# For full integer quantization, though supported types defaults to int8 only, we explicitly declare it for clarity.
+converter.target_spec.supported_types = [tf.int8]
+# These set the input and output tensors to uint8 (added in r2.3)
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+tflite_model = converter.convert()
+
+with open('inceptionv3.tflite', 'wb') as f:
+  f.write(tflite_model)
