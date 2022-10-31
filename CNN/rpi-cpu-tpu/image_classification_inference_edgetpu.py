ff650aa46f2445f9d46654a118a9fd7377d42a9e
HanJeong Lee <54920378+leehanjeong@users.noreply.github.com>
2022-10-31 21:55:16 +0900

    Add comments

--- CNN/rpi-cpu-tpu/image_classification_inference_edgetpu.py
+++ CNN/rpi-cpu-tpu/image_classification_inference_edgetpu.py
@@ -33,7 +33,7 @@ def load_data(input_shape):
     for image_file in input_files:
         image = Image.open(image_path+'/'+image_file)
         image = image.convert('RGB').resize([input_shape[1], input_shape[2]], Image.ANTIALIAS)
-        image = np.array(image)
+        image = np.array(image, dtype=np.uint8)
         image = np.expand_dims(image, axis=0)
         load_data.append(image)
 
@@ -44,19 +44,22 @@ def inference(dataset, interpreter, input_index, top_k, threshold):
     iter_times, accuracy = [], []
 
     for image in dataset:
+        inference_time = time.perf_counter()
         interpreter.set_tensor(input_index, image)
-        start = time.perf_counter() 
         interpreter.invoke()
-        iter_times.append(time.perf_counter() - start)  
 
         classes = classify.get_output(interpreter, top_k, threshold) 
         for klass in classes:
             accuracy.append(klass.score)
+       
+        iter_times.append(time.perf_counter()-inference_time)
 
     return accuracy, iter_times
 
 
 def main():
+  total_time = time.time()
+
   parser = argparse.ArgumentParser(
       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument(
@@ -75,31 +78,40 @@ def main():
   top_k = args.top_k
   threshold = args.threshold
 
+  # Load model
   model_load_time = time.time()
   interpreter = make_interpreter(args.model)
-  input_details = interpreter.get_input_details()
-  input_index = input_details[0]['index']
-  input_shape = input_details[0]['shape']
   interpreter.allocate_tensors()
   model_load_time = time.time() - model_load_time
 
+  # Get input shape
+  input_details = interpreter.get_input_details()
+  input_index = input_details[0]['index']
+  input_shape = input_details[0]['shape']
+
+  # Get dataset
   dataset_load_time=time.time()
   dataset = load_data(input_shape)
   dataset_load_time = time.time() - dataset_load_time
-    
+
+  # Inference
   inference_time = time.time()
   accuracy, iter_times = inference(dataset, interpreter, input_index, top_k, threshold)
   inference_time = time.time() - inference_time
 
+  total_time = time.time() - total_time
+  
+  print(args.model)
   print('***** TF-lite matric *****')
   print('accuracy = {:.3f}'.format(np.sum(accuracy)/(len(dataset)*len(dataset[0]))))
   print('model_load_time = {:.3f}'.format(model_load_time))
   print('dataset_load_time = {:.3f}'.format(dataset_load_time))
   print('inference_time = {:.3f}'.format(inference_time))
-  print('inference_time(avg) = {:.3f}'.format(np.sum(iter_times) / (len(dataset)*len(dataset[0]))))
-  print('IPS = {:.3f}'.format((len(dataset)*len(dataset[0])) / (model_load_time + dataset_load_time + inference_time)))
-  print('IPS(inf) = {:.3f}'.format((len(dataset)*len(dataset[0])) / np.sum(iter_times)))
-  
+  print('inference_time(avg) = {:.3f}'.format(inference_time / (len(dataset)*len(dataset[0]))))
+  print('invoke_time(avg) = {:.4f}'.format(np.sum(iter_times) / (len(dataset)*len(dataset[0]))))
+  print('IPS = {:.3f}'.format((len(dataset)*len(dataset[0])) / total_time))
+  print('IPS(inf) = {:.3f}'.format((len(dataset)*len(dataset[0])) / inference_time))
+  print('total_time = {:.3f}'.format(total_time))
 
 
 if __name__ == '__main__':
