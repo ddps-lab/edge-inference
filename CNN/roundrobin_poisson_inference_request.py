b926f201ea52cbe8d800d0fd4b7069516ecdf4b3
manchann <chl8273@naver.com>
2023-02-13 16:12:48 +0900

    debugging

--- CNN/roundrobin_poisson_inference_request.py
+++ CNN/roundrobin_poisson_inference_request.py
@@ -105,8 +105,8 @@ def ModelRequest(model, data):
 
 
 get_weighted_smooth = roundrobin.smooth(models)
-model_sequence = ''.join([get_weighted_smooth() for _ in range(MAX)])
-
+model_sequence = [].join([get_weighted_smooth() for _ in range(MAX)])
+print(model_sequence)
 RoundPerEvent = 10
 TotalEvent = 100
 poisson_distribution = random.poisson(RoundPerEvent, TotalEvent)
