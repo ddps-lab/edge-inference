9a1adc1100e8b8896fab90ef8958a17e2120cd85
manchann <chl8273@naver.com>
2023-02-17 11:38:08 +0900

    주석 제거

--- CNN/edge_inference_request.py
+++ CNN/edge_inference_request.py
@@ -7,9 +7,10 @@ import os
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
-    tf.config.experimental.set_virtual_device_configuration(gpus[0],
-                                                            [tf.config.experimental.VirtualDeviceConfiguration(
-                                                                memory_limit=0.6 * 1024)])
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(
+            memory_limit=0.6 * 1024)])
 
     # print("1:", psutil.virtual_memory())
 
@@ -53,20 +54,6 @@ def image_preprocess(image_array, model_name):
         image_array[tf.newaxis, ...])
 
 
-# def mobilenetv1_image_preprocess(image_array):
-#     return mobilenet.preprocess_input(
-#         image_array[tf.newaxis, ...])
-#
-#
-# def mobilenetv2_image_preprocess(image_array):
-#     return mobilenet_v2.preprocess_input(
-#         image_array[tf.newaxis, ...])
-#
-#
-# def inceptionv3_image_preprocess(image_array):
-#     return inception_v3.preprocess_input(
-#         image_array[tf.newaxis, ...])
-
 mobilenetv1_image_path = './dataset/imagenet/imagenet_1000_raw/n02782093_1.JPEG'
 mobilenetv2_image_path = './dataset/imagenet/imagenet_1000_raw/n04404412_1.JPEG'
 inceptionv3_image_path = './dataset/imagenet/imagenet_1000_raw/n13040303_1.JPEG'
@@ -78,10 +65,6 @@ inceptionv3_test_image = inception_load_image(inceptionv3_image_path)
 mobilenetv1_test_image_array = image_to_array(mobilenetv1_test_image)
 mobilenetv2_test_image_array = image_to_array(mobilenetv2_test_image)
 inceptionv3_test_image_array = image_to_array(inceptionv3_test_image)
-
-# mobilenetv1_test_image_preprocessed = mobilenetv1_image_preprocess(mobilenetv1_test_image_array)
-# mobilenetv2_test_image_preprocessed = mobilenetv2_image_preprocess(mobilenetv2_test_image_array)
-# inceptionv3_test_image_preprocessed = inceptionv3_image_preprocess(inceptionv3_test_image_array)
 
 mobilenetv1_test_image_preprocessed = image_preprocess(mobilenetv1_test_image_array, 'mobilenet')
 mobilenetv2_test_image_preprocessed = image_preprocess(mobilenetv2_test_image_array, 'mobilenet_v2')
@@ -95,11 +78,6 @@ def save_model(model, saved_model_dir):
 
 
 loaded_models = {}
-
-# for model in models.keys():
-#     path = f'{model}_saved_model'
-#     save_model(model, path)
-#     loaded_models[model] = tf.keras.models.load_model(path)
 
 model_path = 'mobilenet_saved_model'
 model_names = models_detail.keys()
@@ -118,11 +96,13 @@ def mobilenetv1():
     print(result)
     return 'mobilenetv1 inference success'
 
+
 @app.route('/mobilenetv2')
 def mobilenetv2():
     result = loaded_models['mobilenet_v2'].predict(mobilenetv1_test_image_preprocessed)
     print(result)
     return 'mobilenetv2 inference success'
+
 
 @app.route('/inceptionv3')
 def inceptionv3():
