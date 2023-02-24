4797ec2944c16d22b3e88b4746b7d556882f2e6c
kh3654po <kh3654po@gmail.com>
2023-02-24 12:41:53 +0900

    poisson을 이용하여 request를 보내도록 변경

--- CNN/edge_inference_request_scheduler.py
+++ CNN/edge_inference_request_scheduler.py
@@ -1,5 +1,8 @@
 import argparse
 import requests
+import roundrobin
+from numpy import random
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', default='mobilenet,mobilenet_v2,inception_v3', type=str)
@@ -15,13 +18,34 @@ port = args.port
 inference_request_url = f'http://{hostname}:{port}/'
 
 
-def model_reqest(model):
+def model_request(model):
     url = inference_request_url + model
     res = requests.get(url)
 
     return res.text
 
 
-for model in models_to_inference:
-    res = model_reqest(model)
-    print(res)
+models = [('mobilenet', 1), ('mobilenet_v2', 3), ('inception_v3', 6)]
+get_weighted_smooth = roundrobin.smooth(models)
+
+events_avg = 10
+total_event_num = 10
+
+poisson_distribution = random.poisson(events_avg, total_event_num)
+
+for event_num in poisson_distribution:
+    print('request count: ', event_num)
+
+    for idx in event_num:
+        current_inference_model = get_weighted_smooth()
+
+        request_start_time = time.time()
+        res = model_request(current_inference_model)
+        print('total request time: ', time.time() - request_start_time)
+
+        print(res)
+
+
+
+
+
