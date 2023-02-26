1a07c3feb3334518c2d30b6e41e255eaaace6266
kh3654po <kh3654po@gmail.com>
2023-02-26 21:45:10 +0900

    error fix

--- CNN/edge_inference_request_scheduler.py
+++ CNN/edge_inference_request_scheduler.py
@@ -50,8 +50,8 @@ for i in model_edge_info.keys():
     model_edge_info[i] = roundrobin.smooth(model_info)
 
 
-requests_list = ['mobilenet', 'mobilenet_v2', 'mobilenet', 'mobilenet'
-                 'inception_v3', 'mobilenet_v2', 'inception_v3', 'mobilenet'
+requests_list = ['mobilenet', 'mobilenet_v2', 'mobilenet', 'mobilenet',
+                 'inception_v3', 'mobilenet_v2', 'inception_v3', 'mobilenet',
                  'inception_v3', 'mobilenet_v2', 'inception_v3']
 
 threads = []
