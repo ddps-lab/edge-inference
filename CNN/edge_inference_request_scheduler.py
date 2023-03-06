0c42f6019ea26c60f3acdb7af3bb25b5e6dafd17
kh3654po <kh3654po@gmail.com>
2023-03-06 20:36:25 +0900

    엣지장비의 여러 서버로 요청을 전달 할 수있는 구조로 변경

--- CNN/edge_inference_request_scheduler.py
+++ CNN/edge_inference_request_scheduler.py
@@ -7,7 +7,6 @@ from threading import Thread
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--edge', default=None, type=str)
-parser.add_argument('--port', default=5001, type=int)
 
 ######### 임시 코드 ###################
 parser.add_argument('--reqs', default='mobilenet,10', type=str)
@@ -22,18 +21,20 @@ inference_random_flag = temp_args.random
 args = parser.parse_args()
 
 edges_to_inference = args.edge
-port = args.port
 
 
 # 이 부분만 설정하면 모델추가나 장비추가가 수월함. 각 장비의 ip와 로드된 모델들을 설정해주어야함.
-edges_info = {'nvidia-xavier2': {'url': f'http://192.168.0.30:{port}/',
-                                 'model': ['mobilenet', 'mobilenet_v2', 'inception_v3']
+edges_info = {'nvidia-xavier2': {'ip_addr': '192.168.0.30',
+                                 'ports': [5001, 5002],
+                                 'models': ['mobilenet', 'mobilenet_v2', 'inception_v3']
                                  },
-              'nvidia-tx2': {'url': f'http://192.168.0.8:{port}/',
-                             'model': ['mobilenet', 'mobilenet_v2', 'inception_v3']
+              'nvidia-tx2': {'ip_addr': '192.168.0.8',
+                             'ports': [5001],
+                             'models': ['mobilenet', 'mobilenet_v2', 'inception_v3']
                              },
-              'nvidia-nano1': {'url': f'http://192.168.0.29:{port}/',
-                               'model': ['mobilenet']
+              'nvidia-nano1': {'ip_addr': '192.168.0.29',
+                               'ports': [5001],
+                               'models': ['mobilenet']
                                }
               }
 
@@ -59,12 +60,65 @@ models_to_inference = []
 
 for edge_name in edges_to_inference:
     edge_info = edges_info.get(edge_name)
-    models = edge_info.get('model')
+    models = edge_info.get('models')
     models_to_inference.extend(models)
 
 models_to_inference = set(models_to_inference)
 
 print(f'Models to inference: {models_to_inference}')
+
+
+# 현재 스케줄링 방식: 딕셔너리에 모델별로 엣지장비이름 등록, 들어오는 요청에 따라 각 장비들에 라운드로빈으로 스케줄링
+# 문제점 각 모델이 하나씩 들어오면 장비들 중 하나에만 요청이 들어감 -> 고민
+model_edge_info = {}
+
+for edge in edges_to_inference:
+    edge_info = edges_info.get(edge)
+    for model in edge_info.get('models'):
+        if model not in model_edge_info.keys():
+            model_edge_info[model] = []
+
+        model_edge_info[model].append((edge, 1))
+
+print(f'model-edge dataset: {model_edge_info}')
+
+for model in model_edge_info.keys():
+    dataset = model_edge_info.get(model)
+    model_edge_info[model] = roundrobin.smooth(dataset)
+
+
+# 각 엣지 장비별 포트들을 라운드 로빈으로 얻어올 수 있도록 딕셔너리 구성
+edge_port_info = {}
+
+for edge in edges_to_inference:
+    if edge not in edge_port_info.keys():
+        edge_port_info[edge] = []
+
+    edge_info = edges_info.get(edge)
+    for port in edge_info.get('ports'):
+        edge_port_info[edge].append((port, 1))
+
+print(f'ip-edge dataset: {edge_port_info}')
+
+for edge in edge_port_info.keys():
+    dataset = edge_port_info.get(edge)
+    edge_port_info[edge] = roundrobin.smooth(dataset)
+
+
+# 모델로 엣지장비 이름 얻는 함수, 모델마다 엣지장비들의 정보가 담겨 있고 얻어올 때는 라운드로빈으로 순서대로 가져옴
+def get_edge_by_model_rr(model):
+    if model in model_edge_info.keys():
+        return model_edge_info.get(model)()
+    else:
+        return None
+
+
+# 엣지장비 이름으로 해당 포트번호를 얻는 함수, 엣지장비마다 포트 번호 정보가 담겨 있고 라운드로빈으로 순서대로 가져옴
+def get_port_by_edge_rr(edge):
+    if edge in edge_port_info.keys():
+        return edge_port_info.get(edge)()
+    else:
+        return None
 
 
 # 추론을 요청하는 함수, 인자로는 추론을 요청할 엣지 장비, 모델, 요청임. 엣지장비와 모델은 위의 edges_info에 등록되어 있어야함
@@ -77,36 +131,21 @@ def model_request(edge, model, order):
         print(f'[{order}] model must be in {models_to_inference}/ input value: {model}')
         return
 
-    req_processing_start_time = time.time()
     edge_info = edges_info.get(edge)
-    url = edge_info.get('url') + model
+
+    edge_ip_addr = edge_info.get('ip_addr')
+    port = get_port_by_edge_rr(edge)
+    url = f'http://{edge_ip_addr}:{port}/{model}'
+
+    req_processing_start_time = time.time()
     res = requests.get(url)
     processing_time = time.time() - req_processing_start_time
+
     print(f'[{order}] total request time: {processing_time}\n{res.text}')
     return
 
 
-# 현재 스케줄링 방식: 딕셔너리에 모델별로 엣지장비이름 등록, 들어오는 요청에 따라 각 장비들에 라운드로빈으로 스케줄링
-# 문제점 각 모델이 하나씩 들어오면 장비들 중 하나에만 요청이 들어감 -> 고민
-model_edge_info = {}
-
-for edge in edges_to_inference:
-    edge_info = edges_info.get(edge)
-    for model in edge_info.get('model'):
-        if model not in model_edge_info.keys():
-            model_edge_info[model] = []
-
-        model_edge_info[model].append((edge, 1))
-
-print(f'model-edge dataset: {model_edge_info}')
-
-
-for model in model_edge_info.keys():
-    dataset = model_edge_info.get(model)
-    model_edge_info[model] = roundrobin.smooth(dataset)
-
-
-# 들어오는 요청들 임시 코드임!!!
+### 들어오는 요청들 임시 코드임!!! ###
 requests_list = []
 for idx in range(0, len(inference_requests), 2):
     model = inference_requests[idx]
@@ -117,15 +156,18 @@ for idx in range(0, len(inference_requests), 2):
 
 if inference_random_flag:
     random.shuffle(requests_list)
+##############################
 
 
+# 요청을 각 장비에 전달, 여러요청을 동시에 다룰 수 있도록 쓰레드 이용
 threads = []
 order = 0
+
 for req in requests_list:
-    if req in model_edge_info.keys():
-        edge_to_inference = model_edge_info.get(req)()
-    else:
-        edge_to_inference = ''
+    edge_to_inference = get_edge_by_model_rr(req)
+    if edge_to_inference is None:
+        print(f'{req} can\'t be inference')
+        continue
 
     order += 1
     th = Thread(target=model_request, args=(edge_to_inference, req, f'{order}:{edge_to_inference}/{req}'))
