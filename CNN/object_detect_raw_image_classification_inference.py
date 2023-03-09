f295a68938959ceb0f1294dcb130c93887270752
Jungae Park <46885199+jungae-park@users.noreply.github.com>
2023-03-09 16:25:40 +0900

    Update object_detect_raw_image_classification_inference.py
    code import path change

--- CNN/object_detect_raw_image_classification_inference.py
+++ CNN/object_detect_raw_image_classification_inference.py
@@ -12,12 +12,12 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from models.common import DetectMultiBackend
-from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
-from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+from model.yolov5.models.common import DetectMultiBackend
+from model.yolov5.utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from model.yolov5.utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
-from utils.plots import Annotator, colors, save_one_box
-from utils.torch_utils import select_device, smart_inference_mode
+from model.yolov5.utils.plots import Annotator, colors, save_one_box
+from model.yolov5.utils.torch_utils import select_device, smart_inference_mode
 
 
 @smart_inference_mode()
